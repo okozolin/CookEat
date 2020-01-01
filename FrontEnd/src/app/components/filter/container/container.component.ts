b54da93a342ea8261c11839dea78d4a40b93d2e9
Orit Kozolin <orit@nachum.co.il>
2020-01-01 19:07:45 +0200

    container and search componets base

---
+++ FrontEnd/src/app/components/filter/container/container.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-filtercontainer",
+  templateUrl: "/components/filter/container/container.component.html"
+})
+export class FilterContainerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
