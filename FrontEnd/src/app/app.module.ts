66aa27e1855bf158e73618457015e93b36639a53
Orit Kozolin <orit@nachum.co.il>
2020-01-01 20:12:25 +0200

    add Angular Material

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { MatInputModule } from "@angular/material";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainComponent } from "./components/main/main.component";
 import { SearchBoxComponent } from "./components/filter/searchbox/searchbox.component";
 import { FilterContainerComponent } from "./components/filter/container/container.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,12 @@ import { FilterContainerComponent } from "./components/filter/container/containe
     FilterContainerComponent,
     SearchBoxComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
