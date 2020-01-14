aac608d0cfd4f2258fd1e80be52aec533b63ffd3
Orit Kozolin <orit@nachum.co.il>
2020-01-14 17:37:01 +0200

    date picker

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from "@angular/core";
 import {
   MatInputModule,
   MatCardModule,
-  MatButtonModule
+  MatButtonModule,
+  MatNativeDateModule
 } from "@angular/material";
 import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -13,6 +16,7 @@ import { MainComponent } from "./components/main/main.component";
 import { SearchBoxComponent } from "./components/filter/searchbox/searchbox.component";
 import { FilterContainerComponent } from "./components/filter/container/container.component";
 import { TypeCheckBoxesComponent } from "./components/filter/type/type.component";
+import { DatePickerComponent } from "./components/filter/datepicker/datepicker.component";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
@@ -22,7 +26,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     MainComponent,
     FilterContainerComponent,
     SearchBoxComponent,
-    TypeCheckBoxesComponent
+    TypeCheckBoxesComponent,
+    DatePickerComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +36,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     MatCardModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
     BrowserAnimationsModule
   ],
   providers: [],
