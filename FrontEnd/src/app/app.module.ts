3cb4f18f93f0fae6f19b386aa47b5ecdcbf8aef5
Orit Kozolin <orit@nachum.co.il>
2020-01-14 16:15:08 +0200

    add cuisine type checkbox component with material style

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainComponent } from "./components/main/main.component";
 import { SearchBoxComponent } from "./components/filter/searchbox/searchbox.component";
 import { FilterContainerComponent } from "./components/filter/container/container.component";
+import { TypeCheckBoxesComponent } from "./components/filter/type/type.component";
+
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
@@ -14,12 +17,14 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     AppComponent,
     MainComponent,
     FilterContainerComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    TypeCheckBoxesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatInputModule,
+    MatCheckboxModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ FrontEnd/src/app/components/filter/type/type.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-type",
+  templateUrl: "type.component.html"
+})
+export class TypeCheckBoxesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
