a2fcbc5a8fb50442832c37d0be3955e9e9d3eed3
Orit Kozolin <orit@nachum.co.il>
2020-01-14 16:47:11 +0200

    style search box

--- FrontEnd/src/app/components/filter/container/container.component.ts
+++ FrontEnd/src/app/components/filter/container/container.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-filtercontainer",
-  templateUrl: "container.component.html"
+  templateUrl: "container.component.html",
+  styleUrls: ["container.component.css"]
 })
 export class FilterContainerComponent implements OnInit {
   constructor() {}
--- FrontEnd/src/app/components/filter/searchbox/searchbox.component.ts
+++ FrontEnd/src/app/components/filter/searchbox/searchbox.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-searchbox",
-  templateUrl: "searchbox.component.html"
+  templateUrl: "searchbox.component.html",
+  styleUrls: ["searchbox.component.css"]
 })
 export class SearchBoxComponent implements OnInit {
   constructor() {}
