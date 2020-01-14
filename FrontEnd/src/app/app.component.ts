77e6a0e7240f4558e94bd726abded8fec117c2b6
hofap <hofap22@gmail.com>
2020-01-14 20:55:03 +0200

    fix changes2

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -5,11 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
+
 export class AppComponent {
 }
-
-
-=======
-export class AppComponent {}
->>>>>>> e22f825f4108097b68b8623c310bcc7d90cff69f
