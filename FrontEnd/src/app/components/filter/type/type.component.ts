b83de8d42130859c13c9a407ddb4003c744fd7c3
Orit Kozolin <orit@nachum.co.il>
2020-02-10 10:12:24 +0200

    loop over food type

--- FrontEnd/src/app/components/filter/type/type.component.ts
+++ FrontEnd/src/app/components/filter/type/type.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from "@angular/core";
+import { IFoodTypeStatus } from './IFoodTypeStatus.interface'
 
 @Component({
   selector: "app-type",
-  templateUrl: "type.component.html"
+  templateUrl: "type.component.html",
+  styleUrls: ["type.component.css"]
 })
 export class TypeCheckBoxesComponent implements OnInit {
+  cuisineTypes = [
+    'Indian',
+    'Chinese',
+    'Italian',
+    'Japanese',
+    'Jewish',
+    'American',
+    'French',
+    'Israeli'
+  ];
+  cuisineTypesChecked: IFoodTypeStatus[] = [];
   constructor() {}
 
   ngOnInit() {}
