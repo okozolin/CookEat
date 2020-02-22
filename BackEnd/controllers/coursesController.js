e5f3c361e8f9a5b1b8646745cd437e4bac48a715
TamarAdmoni <51236376+TamarAdmoni@users.noreply.github.com>
2020-02-22 16:25:49 +0200

    filter data by food type

--- BackEnd/controllers/coursesController.js
+++ BackEnd/controllers/coursesController.js
@@ -12,6 +12,9 @@
 				var limit = req.query["limit"];
 				var offset = req.query["offset"];
 				var sortBy = req.query["sortBy"];
+				var foodType = req.query["foodType"];
+
+				console.log("query:", req.query, "foodType", foodType);
 
 				data.getCourses(
 
@@ -19,10 +22,10 @@
 						if (err) {
 							res.send(400, err);
 						} else {
-							
+
 							res.send(courses);
 						}
-					}, limit, offset, sortBy);
+					}, limit, offset, sortBy, foodType);
 			});
 	};
 })(module.exports);
--- BackEnd/data/seedData.js
+++ BackEnd/data/seedData.js
@@ -1,25 +1,61 @@
-﻿(function(seedData) {
+﻿(function (seedData) {
 	seedData.initialCourses = [
 		{
-			Course: "Course1",
-			owner: "owner1"
-			
+			courseName: "course6",
+			owner: "owner6",
+			date: "2020 - 09 - 23T18: 25: 43.511Z",
+			foodType: "Vietnamese"
 		}, {
-			Course: "Course2",
-			owner: "owner2"
-			
+			courseName: "course2",
+			owner: "owner2",
+			date: "2020 - 06 - 23T18: 25: 43.511Z",
+			foodType: "Mexican"
 		}, {
-			Course: "Course3",
-			owner: "owner3"
-			
+			courseName: "course3",
+			owner: "owner3",
+			date: "2020 - 04 - 07T18: 25: 43.511Z",
+			foodType: "Chinese"
 		}, {
-			Course: "Course4",
-			owner: "owner4"
-			
+			courseName: "course4",
+			owner: "owner4",
+			date: "2012 - 05 - 05T20: 25: 43.511Z",
+			foodType: "Mexican"
+
 		}, {
-			Course: "Course5",
-			owner: "owner5"
-			
+			courseName: "course5",
+			owner: "owner5",
+			date: "2020 - 06 - 18T18: 25: 43.511Z",
+			foodType: "Indian"
+
+		}, {
+			courseName: "course7",
+			owner: "owner7",
+			date: "2020 - 05 - 23T18: 25: 43.511Z",
+			foodType: "Mediterranean"
+
+		}, {
+			courseName: "course9",
+			owner: "owner9",
+			date: "2020 - 03 - 12T18: 25: 43.511Z",
+			foodType: "Indian"
+
+		}, {
+			courseName: "course8",
+			owner: "owner8",
+			date: "2020 - 04 - 10T18: 25: 43.511Z",
+			foodType: "Italian"
+
+		}, {
+			courseName: "course0",
+			owner: "owner0",
+			date: "2020 - 05 - 23T18: 25: 43.511Z",
+			foodType: "French"
+
+		}, {
+			courseName: "course1",
+			owner: "owner1",
+			date: "2020 - 04 - 23T18: 25: 43.511Z",
+			foodType: "Greek"
 		}]
-	;
+		;
 })(module.exports);
