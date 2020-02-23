c4ce8a9961a31e300c5d594c4d529d2503262e20
Orit Kozolin <orit@nachum.co.il>
2020-02-23 10:59:54 +0200

    pass data from child component to parent component

--- FrontEnd/src/app/components/filter/container/container.component.ts
+++ FrontEnd/src/app/components/filter/container/container.component.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup} from "@angular/forms";
+import {AfterViewInit, Component, ViewChild} from "@angular/core";
+import { SearchBoxComponent } from '../searchbox/searchbox.component'
+import { TypeCheckBoxesComponent } from '../type/type.component'
+import { DatePickerComponent } from "../datepicker/datepicker.component";
 
 @Component({
   selector: "app-filtercontainer",
   templateUrl: "container.component.html",
   styleUrls: ["container.component.css"]
 })
-export class FilterContainerComponent implements OnInit {
-  filterForm: FormGroup;
+export class FilterContainerComponent implements AfterViewInit {
 
-  constructor(private fb: FormBuilder) {}
+  constructor() {}
 
-  ngOnInit() {
-    console.log("this.fb", this.fb)
-    this.filterForm = this.fb.group({
-      searchBox: [''],
-      foodType: [''],
-      datePicker: ['']
-    })
+  searchBoxFromComp: string;
+  @ViewChild(SearchBoxComponent) childSearchText;
+
+  typeCheckboxesFromComp: [] ;
+  @ViewChild(TypeCheckBoxesComponent) childTypeCheckboxesArr;
+
+  dateFromComp: string;
+  @ViewChild(DatePickerComponent) childDate;
+
+  ngAfterViewInit() {
+    this.searchBoxFromComp = this.childSearchText.message;
+    this.typeCheckboxesFromComp = this.childTypeCheckboxesArr.selectedTypesValues;
+    this.dateFromComp = this.childDate.message;
   }
 
   search() {
-    console.log("this.fb", this.fb)
+    this.searchBoxFromComp = this.childSearchText.message;
+    this.typeCheckboxesFromComp = this.childTypeCheckboxesArr.selectedTypesValues;
+    this.dateFromComp = this.childDate.message;
 
-    console.log('this.customerForm', this.filterForm);
-    console.log('pressed search: ' + JSON.stringify(this.filterForm.value));
+    console.log("this.childTypeCheckboxesArr.selectedTypesValues", this.childTypeCheckboxesArr.selectedTypesValues)
+    console.log("this.typeCheckboxesFromComp", this.typeCheckboxesFromComp)
+    // console.log('pressed search: ' + JSON.stringify(this.filterForm.value));
   }
 
 }
