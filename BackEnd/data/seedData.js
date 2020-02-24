e247e0ef9ff40e40048ceac9046da1f366fe6a45
TamarAdmoni <51236376+TamarAdmoni@users.noreply.github.com>
2020-02-24 18:02:56 +0200

    add stub properties

--- BackEnd/data/seedData.js
+++ BackEnd/data/seedData.js
@@ -2,60 +2,115 @@
 	seedData.initialCourses = [
 		{
 			courseName: "course6",
-			owner: "owner6",
+			tutorName: "Morin smith",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Vietnamese"
+			foodType: "Vietnamese",
+			location: "Tel Aviv",
+			price: 400,
+			rank: 5,
+			imagePath: "https://i.udemycdn.com/course/240x135/1974482_9e10_3.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course2",
-			owner: "owner2",
+			tutorName: "owner2",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Mexican"
+			foodType: "Mexican",
+			location: "Jerusalem",
+			price: 350,
+			rank: 4.9,
+			imagePath: "https://i.udemycdn.com/course/240x135/1423592_5992.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course3",
-			owner: "owner3",
+			tutorName: "owner3",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Chinese"
+			foodType: "Chinese",
+			location: "Tel Aviv",
+			price: 370,
+			rank: 3.9,
+			imagePath:"https://i.udemycdn.com/course/240x135/2612464_8c36_3.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course4",
-			owner: "owner4",
+			tutorName: "owner4",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Mexican"
-
+			foodType: "Mexican",
+			location: "Petah Tikva",
+			price: 410,
+			rank: 3.5,
+			imagePath: "https://i.udemycdn.com/course/240x135/1563558_03fb_2.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course5",
-			owner: "owner5",
+			tutorName: "owner5",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Indian"
-
+			foodType: "Indian",
+			location: "Ramat Gan",
+			price: 320,
+			rank: 4.5,
+			imagePath:"https://i.udemycdn.com/course/240x135/1788646_4be5.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course7",
-			owner: "owner7",
+			tutorName: "owner7",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Mediterranean"
-
+			foodType: "Mediterranean",
+			location: "Ramat Gan",
+			price: 390,
+			rank: 4.2,
+			imagePath:"https://i.udemycdn.com/course/240x135/1006418_23ec_6.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course9",
-			owner: "owner9",
+			tutorName: "owner9",
 			startDate: "2020-03-12T18:25:43.511Z",
-			foodType: "Indian"
-
+			foodType: "Indian",
+			location: "Beer Sheva",
+			price: 370,
+			rank: 4.8,
+			imagePath:"https://i.udemycdn.com/course/240x135/465334_4dc1_8.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course8",
-			owner: "owner8",
+			tutorName: "owner8",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Italian"
-
+			foodType: "Italian",
+			location: "Jerusalem",
+			price: 400,
+			rank: 5,
+			imagePath:"https://i.udemycdn.com/course/240x135/1188824_8def.jpg",
+			duration: 4,
+			description: ""
 		}, {
 			courseName: "course0",
-			owner: "owner0",
+			tutorName: "owner0",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "French"
+			foodType: "French",
+			location: "Hulon",
+			price: 300,
+			rank: 4.6,
+			imagePath: "https://i.udemycdn.com/course/240x135/1751546_58f9_2.jpg",
+			duration: 4,
+			description: ""
 
 		}, {
 			courseName: "course1",
-			owner: "owner1",
+			tutorName: "owner1",
 			startDate: "2020-04-11T10:20:30Z",
-			foodType: "Greek"
+			foodType: "Greek",
+			location: "Tel Aviv",
+			price: 350,
+			rank: 4.1,
+			imagePath: "https://i.udemycdn.com/course/240x135/626076_2ade_3.jpg",
+			duration: 4,
+			description: ""
 		}]
 		;
 })(module.exports);
